e55ec3dc6225c5ee2cf1bf5eca211794f5005726
sashkozab <pirogovich13@gmail.com>
2016-02-08 15:36:18 +0200

    corrected setup and requirements info

--- setup.py
+++ setup.py
@@ -37,6 +37,8 @@ setup(
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
