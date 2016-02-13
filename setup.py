23aba4ffe47b45f50480a6913410b4136796fb58
sashkozab <pirogovich13@gmail.com>
2016-02-13 17:34:27 +0200

    added xlsxwriter,numpy to requires

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.rst", "rb") as f:
 setup(
     name = "hydroparser",
     packages = ["hydroparser"],
-    install_requires = ['openpyxl','win-unicode-console'],
+    install_requires = ['openpyxl','win-unicode-console','xlsxwriter','numpy'],
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
